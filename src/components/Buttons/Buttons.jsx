ab23c21013808b9a0610ce0ecb50eba417020bf7
ashay <ashaysoda24@gmail.com>
2021-08-13 23:58:57 +0530

    "Test Modal"

--- src/components/Buttons/Buttons.jsx
+++ src/components/Buttons/Buttons.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import './buttons.css'
+import MyModal from '../myModal/MyModal'
+import { useState } from 'react';
 
-export default function Buttons({source, text, btnStyle, width, borderRadius, mainbtn}) {
+export default function Buttons({ source, text, btnStyle, width, borderRadius, mainbtn }) {
 
-    
+    const [modalShow, setModalShow] = useState(false);
+
 
     return (
-        <div className ={`buttons ${mainbtn}`}>
+        <div className={`buttons ${mainbtn}`}>
             {
-                btnStyle ? <button className={`${width} ${borderRadius}`}>{text} <img src={source} alt="" /></button> : <button style={{color:"white",backgroundColor:"#2F6CE5"}}><img src={source} className="me-2" alt="" />{text}</button>
-                
+                btnStyle ? <button className={`${width} ${borderRadius}`}>{text} <img src={source} alt="" /></button> : <button onClick={() => setModalShow(true)} style={{ color: "white", backgroundColor: "#2F6CE5" }}><img src={source} className="me-2" alt="" />{text}</button>
             }
+            <MyModal show={modalShow}
+                onHide={() => setModalShow(false)} />
 
 
         </div>
--- src/components/pageSelect/PageSelect.jsx
+++ src/components/pageSelect/PageSelect.jsx
@@ -7,34 +7,37 @@ import joinGroupIcon from '../../images/joinGroupIcon.png'
 import { Link } from 'react-router-dom';
 
 export default function PageSelect() {
+
+    
+
     return (
         <>
             <Container fluid className="cont">
                 <Container>
-                <Navbar bg="white" expand="lg" className="pageSelect">
+                    <Navbar bg="white" expand="lg" className="pageSelect">
+                        <Navbar.Collapse id="navbarScroll">
+                            <Nav
+                                className="mr-auto my-2 my-lg-0"
+                                style={{ maxHeight: '100px' }}
+                                navbarScroll
+                                activeKey="/"
 
-                    <Navbar.Collapse id="navbarScroll">
-                        <Nav
-                            className="mr-auto my-2 my-lg-0"
-                            style={{ maxHeight: '100px' }}
-                            navbarScroll
-                            activeKey="/"
-                            
-                        >
-                            {/* <Link to="/" className="pageChoice">All Posts(32)</Link>
+                            >
+                                {/* <Link to="/" className="pageChoice">All Posts(32)</Link>
                             <Link className="article" to="/article">Article</Link> */}
-                            <Nav.Link href="/" >All Posts(32)</Nav.Link>
-                            <Nav.Link href="/article">Article</Nav.Link>
-                            <Nav.Link href="#action3">Even</Nav.Link>
-                            <Nav.Link href="#action4">Education</Nav.Link>
-                            <Nav.Link href="#action5">Job</Nav.Link>
-                        </Nav>
-                        <Navbar.Collapse className="justify-content-end">
-                            <Buttons source ={dropdown} text="Write a Post" btnStyle= {true} color="black"/>
-                            <Buttons source ={joinGroupIcon} text="Join Group" btnStyle= {false} color="white" bg="#2F6CE5"/>
+                                <Nav.Link href="/" >All Posts(32)</Nav.Link>
+                                <Nav.Link href="/article">Article</Nav.Link>
+                                <Nav.Link href="#action3">Even</Nav.Link>
+                                <Nav.Link href="#action4">Education</Nav.Link>
+                                <Nav.Link href="#action5">Job</Nav.Link>
+                            </Nav>
+                            <Navbar.Collapse className="justify-content-end">
+                                <Buttons source={dropdown} text="Write a Post" btnStyle={true} color="black" />
+                                <Buttons  source={joinGroupIcon} text="Join Group" btnStyle={false}/>
+                                
+                            </Navbar.Collapse>
                         </Navbar.Collapse>
-                    </Navbar.Collapse>
-                </Navbar>
+                    </Navbar>
                 </Container>
             </Container>
         </>
